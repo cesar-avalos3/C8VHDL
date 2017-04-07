7f6ae19494c8216b7fff59c19ff7c43d92e877de
Cesar Avalos <cesar.avalos3@gmail.com>
2017-04-07 12:44:39 -0500

    Implement crappy PC
    Implementation of a crummy program counter

---
+++ C8VHDL.srcs/sources_1/new/programCounter.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/07/2017 12:33:23 PM
+-- Design Name: 
+-- Module Name: programCounter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.all;
+
+-- Using unsigned as the memory addresses are going to be unsigned.
+
+entity programCounter is
+    Port ( clk, enable, load, increment, reset: IN STD_LOGIC;
+           dataBus   : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+           addressBus: OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+           );
+end programCounter;
+
+-- To put in simple terms, the PC tells the CPU which address its reading
+-- If reset is on, then return to 512, place where the program usually starts in CHIP8
+-- If load is on, then whatever is on the dataBus is now the PC
+-- If increment is on, the increase the current address by 1 byte.
+
+architecture Behavioral of programCounter is
+
+signal currentRegister: STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+begin
+process(clk, reset) begin
+
+    if(reset = '1') then
+        currentRegister <= x"200";        
+    elsif(rising_edge(clk)) then
+        --Should increase have priority?
+        if(increment = '1') then
+            currentRegister <= currentRegister + x"0001";
+        elsif(load = '1') then
+            currentRegister <= dataBus;
+        end if;
+    end if;
+end process;
+    addressBus <= currentRegister when enable = '1' else "ZZZZZZZZZZZZZZZZ";
+    
+end Behavioral;
